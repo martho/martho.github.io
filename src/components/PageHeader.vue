968797eb71e917fe9bc07b1a02df47821869340d
Markus Thode <markus.thode@etecture.de>
2020-05-04 00:41:48 +0200

    Add PageHeader component

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,40 @@
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
+
+<script>
+export default {
+    name: 'PageHeader'
+}
+</script>
+
+<template>
+    <div class="pageHeader">
+        <header class="header">
+            <strong>
+                <g-link to="/">{{ $static.metadata.siteName }}</g-link>
+            </strong>
+            <nav class="nav">
+                <g-link class="nav__link" to="/">Home</g-link>
+            </nav>
+        </header>
+    </div>
+</template>
+
+<style scoped>
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+  height: 80px;
+}
+
+.nav__link {
+  margin-left: 20px;
+}
+</style>
